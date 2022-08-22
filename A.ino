83b030c98ccf529b12599414ee2cec199542fcf9
0xTurk <97168241+TurkTheLegend@users.noreply.github.com>
2022-08-22 10:04:59 +0700

    Add files via upload

--- A.ino
+++ A.ino
@@ -1,6 +1,6 @@
 #include<ipst.h>
-int R = 17 ,Y = 19,G = 20 ,speaker = 16,result;
-int t = 0,c = 0;
+const int R = 17 ,Y = 19,G = 20 ,speaker = 16;
+int t = 0,c = 1 ,result;
 int pattern[] = {0b11111111,0b01111111,0b00111111,0b00011111,0b00001111,0b00000111,0b00000011,0b00000001};
 bool Day = true;
 void setup() {
@@ -9,146 +9,144 @@ void setup() {
 }
 
 void loop() {
-  if(0 <= t && t <= 198 && Day == true){
-    if(t<=40){
-      if(c == 0){beep(speaker);}
-      c++;
+  if(sw_1() && Day == true){Day = false; t = 0; c = 1;}
+  if(sw_OK() && Day == false){Day = true; t = 0; c = 1;}
+  if((t >= 198 && Day == true)){t = 0; c = 1;}
+  if((t >= 103 && Day == false)){t = 0; c = 1;}
+  if(0 <= t && t <= 197 && Day == true){
+    if(t<40){  
+      if(c == 1){beep(speaker);}
       result = 40 - c ;
+      c++;  
       out(R,0);
       out(Y,0);
       out(G,1);
-      if(40 == c){c = 0;}
       glcd(0,0,"%d",result);
-      if(t <= 5) {
-        LED8(pattern[0]);}
-      else if (t <= 10){
-        LED8(pattern[1]);}
-      else if (t <= 15){
-        LED8(pattern[2]);}
-      else if (t <= 20){
-        LED8(pattern[3]);}
-      else if (t <= 25){
-        LED8(pattern[4]);}
-      else if (t <= 30){
-        LED8(pattern[5]);}
-      else if (t <= 35){
-        LED8(pattern[6]);}
-      else if (t <= 40){
+      if(result < 5) {
         LED8(pattern[7]);}
+      else if (result < 10){
+        LED8(pattern[6]);}
+      else if (result < 15){
+        LED8(pattern[5]);}
+      else if (result < 20){
+        LED8(pattern[4]);}
+      else if (result < 25){
+        LED8(pattern[3]);}
+      else if (result < 30){
+        LED8(pattern[2]);}
+      else if (result < 35){
+        LED8(pattern[1]);}
+      else if (result < 40){
+        LED8(pattern[0]);}
     }
-    else if (t<=42){
-      if(c == 0){beep(speaker);}
-      c++;
+    else if (t<42){
+      if(t == 40){c = 1;}
+      if(c == 1){beep(speaker);}
       result = 2-c;
+      c++;
       out(R,0);
       out(Y,1);
       out(G,0);
-      if(2 <= c){c = 0;}
       glcd(0,0,"%d",result);
-      if (t <= 41){
-        LED8(pattern[0]);}
-      else if (t <= 42) {
+      if (result < 1){
         LED8(pattern[4]);}
+      else if (result < 2) {
+        LED8(pattern[0]);}
     }
     else {
-      if(c == 0){beep(speaker);}
-      c++;
+      if(t == 42){c = 1;}
+      if(c == 1){beep(speaker);}
       result = 156 - c;
+      c++;
       out(R,1);
       out(Y,0);
       out(G,0);
-      if(156 <= c){c = 0;}
       glcd(0,0,"%d",result);
-      if(t <= 62){
-        LED8(pattern[0]);}
-      else if (t <= 81){
-        LED8(pattern[1]);}
-      else if (t <= 101){
-        LED8(pattern[2]);}
-      else if (t <= 120){
-        LED8(pattern[3]);}
-      else if (t <= 140){
-        LED8(pattern[4]);}
-      else if (t <= 159){
-        LED8(pattern[5]);}
-      else if (t <= 179){
-        LED8(pattern[6]);}
-      else if (t <= 198){
+      if(result < 20){
         LED8(pattern[7]);}
+      else if (result < 39){
+        LED8(pattern[6]);}
+      else if (result < 59){
+        LED8(pattern[5]);}
+      else if (result < 78){
+        LED8(pattern[4]);}
+      else if (result < 98){
+        LED8(pattern[3]);}
+      else if (result < 117){
+        LED8(pattern[2]);}
+      else if (result < 137){
+        LED8(pattern[1]);}
+      else if (result < 156){
+        LED8(pattern[0]);}
     }
   }
-  else if (0 <= t && t <= 103 && Day == false){
-    if (t <= 20){
-    if(c == 0){beep(speaker);}
-    c++;
-    result = 20 - c;
-    out(R,0);
-    out(Y,0);
-    out(G,1);
-    if(20 <= c){c = 0;}
-    glcd(0,0,"%d",result);
-    if(t <= 3) {
-        LED8(pattern[0]);}
-      else if (t <= 5){
-        LED8(pattern[1]);}
-      else if (t <= 8){
-        LED8(pattern[2]);}
-      else if (t <= 10){
-        LED8(pattern[3]);}
-      else if (t <= 13){
-        LED8(pattern[4]);}
-      else if (t <= 15){
-        LED8(pattern[5]);}
-      else if (t <= 18){
-        LED8(pattern[6]);}
-      else if (t <= 20){
-        LED8(pattern[7]);}
-    }
-    else if (t <= 22){
-      if(c == 0){beep(speaker);}
+  else if (0 <= t && t <= 102 && Day == false){
+    if (t < 20){
+    if(c == 1){beep(speaker);}
+      result = 20 - c;
       c++;
+      out(R,0);
+      out(Y,0);
+      out(G,1);
+      glcd(0,0,"%d",result);
+      if(result < 3) {
+        LED8(pattern[7]);}
+      else if (result < 5){
+        LED8(pattern[6]);}
+      else if (result < 8){
+        LED8(pattern[5]);}
+      else if (result < 10){
+        LED8(pattern[4]);}
+      else if (result < 13){
+        LED8(pattern[3]);}
+      else if (result < 15){
+        LED8(pattern[2]);}
+      else if (result < 18){
+        LED8(pattern[1]);}
+      else if (result < 20){
+        LED8(pattern[0]);}
+    }
+    else if (t < 22){
+      if(t == 20){c = 1;}
+      if(c == 1){beep(speaker);}
       result = 2-c;
+      c++;
       out(R,0);
       out(Y,1);
       out(G,0);
-      if(2 <= c){c = 0;}
       glcd(0,0,"%d",result);
-      if (t <= 21){
-        LED8(pattern[0]);}
-      else if (t <= 22) {
+      if (result < 1){
         LED8(pattern[4]);}
+      else if (result < 2) {
+        LED8(pattern[0]);}
     }    
     else {
-      if(c == 0){beep(speaker);}
-      c++;
+      if(t == 22){c = 1;}
+      if(c == 1){beep(speaker);}
       result = 81 - c;
+      c++;
       out(R,1);
       out(Y,0);
       out(G,0);
-      if(81 <= c){c = 0;}
       glcd(0,0,"%d",result);
-      if(t <= 32){
-        LED8(pattern[0]);}
-      else if (t <= 42){
-        LED8(pattern[1]);}
-      else if (t <= 52){
-        LED8(pattern[2]);}
-      else if (t <= 63){
-        LED8(pattern[3]);}
-      else if (t <= 73){
-        LED8(pattern[4]);}
-      else if (t <= 83){
-        LED8(pattern[5]);}
-      else if (t <= 93){
-        LED8(pattern[6]);}
-      else if (t <= 103){
+      if(result < 10){
         LED8(pattern[7]);}
+      else if (result < 20){
+        LED8(pattern[6]);}
+      else if (result < 30){
+        LED8(pattern[5]);}
+      else if (result < 41){
+        LED8(pattern[4]);}
+      else if (result < 51){
+        LED8(pattern[3]);}
+      else if (result < 61){
+        LED8(pattern[2]);}
+      else if (result < 71){
+        LED8(pattern[1]);}
+      else if (result < 81){
+        LED8(pattern[0]);}
     }
   }
-  if(sw_1() && Day == true){Day = false; t = 0; c = 0;}
-  if(sw_OK() && Day == false){Day = true; t = 0; c = 0;}
-  if((t >= 198 && Day == true)){t = 0; c = 0;}
-  if((t >= 103 && Day == false)){t = 0; c = 0;}
   t++;
   delay(1000);
   glcdClear();
